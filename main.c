873bfaa25687d58d4fe17825be4ec344b11fa120
danila <danila@localhost.localdomain>
2020-02-26 10:22:39 +0700

    minor updates

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include "f.h"//  point ; circle; check_oper;
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char all[60];
+    printf("Введите входные данные формата:\n");
+    printf("circle(x y, r) - для круга\n");
+    fgets(all, 60, stdin);
+    check_oper(all);
 }
